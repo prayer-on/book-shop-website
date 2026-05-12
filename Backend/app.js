de1026eed6db7d06a39e62b4e79e452cf7e69252
prayer-on <prayer.onaiwu@gmail.com>
2026-05-12 17:28:21 +0200

    fix: correct cors origin url

--- Backend/app.js
+++ Backend/app.js
@@ -19,15 +19,12 @@ console.log('Successfully connected to MongoDB Atlas!')
 });
 
 // CORS ⬇️
-app.use((req, res, next) => {
-  res.setHeader('Access-Control-Allow-Origin', '*');
-  res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
-  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
-  next();
-});
-
-app.use(cors())
-
+// CORS 🛡️
+app.use(cors({
+  origin: 'https://book-shop-website-frontend.onrender.com',
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS'],
+  allowedHeaders: ['Content-Type', 'Authorization', 'Origin', 'Accept']
+}));
 
 
 // BODY PARSER ⬇️
